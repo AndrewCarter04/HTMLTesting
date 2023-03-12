cec4983d8c3a800639c20ecdd7aa1393ec32c783
Andrew Carter <68480855+AndrewCarter04@users.noreply.github.com>
2023-03-12 00:45:42 +0000

    Add image features

--- scripts.js
+++ scripts.js
@@ -14,3 +14,24 @@ function submitButton1() {
     div.querySelector("#text1").value = "";
 
 }
+
+function submitButton2() {
+
+    var div = document.getElementById("divTesting1");
+    var imgURL = div.querySelector("#text2").value;
+    var img = div.querySelector("#outputImg2");
+
+    img.setAttribute("src", imgURL);
+
+}
+
+function submitButton3() {
+
+    var div = document.getElementById("divTesting1");
+    var img = div.querySelector("#outputImg3");
+
+    img.setAttribute("src", div.querySelector("#outputImg2").getAttribute("src"));
+    img.setAttribute("width", div.querySelector("#num3-1").value);
+    img.setAttribute("height", div.querySelector("#num3-2").value);
+
+}
